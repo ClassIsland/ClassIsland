97e298cb52d006029b1a9402488934f6a5e2a9d0
WRC <hello_wrc@outlook.com>
2023-08-14 14:48:24 +0800

    #2 设置界面、档案编辑界面添加AppBar

---
+++ ClassIsland.UnitTests/NativeHelperTest.cs
@@ -0,0 +1,20 @@
+﻿namespace ClassIsland.UnitTests;
+
+[TestClass]
+public class NativeHelperTest
+{
+    [TestMethod]
+    public void TestIsOccupiedTrue()
+    {
+        var v = NativeWindowHelper.IsOccupied(Environment.ProcessPath!);
+        Assert.IsTrue(v);
+    }
+
+    [TestMethod]
+    public void TestIsOccupiedFalse()
+    {
+        File.WriteAllText("./test.txt", "test");
+        var v = NativeWindowHelper.IsOccupied("./test.txt");
+        Assert.IsFalse(v);
+    }
+}
