67302d69b3dcc6068789c39bb4d0ac376e37593f
WRC <hello_wrc@outlook.com>
2023-09-01 17:00:32 +0800

    小优化

---
+++ ClassIsland/Converters/MiniInfoGuidToMiniInfoProviderMultiConverter.cs
@@ -0,0 +1,23 @@
+﻿using ClassIsland.Interfaces;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ClassIsland.Converters;
+
+public class MiniInfoGuidToMiniInfoProviderMultiConverter : IMultiValueConverter
+{
+    public object? Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+    {
+        var guid = (string?)values[0];
+        if (guid == null) return null;
+        var providers = (ObservableDictionary<string, IMiniInfoProvider>)values[1];
+        if (!providers.ContainsKey(guid)) return null;
+        return providers[guid];
+    }
+
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+    {
+        return new object[] { };
+    }
+}
