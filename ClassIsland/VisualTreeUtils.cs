ce5f6279475ba54eea3bccb6223a8caaed9e6907
WRC <hello_wrc@outlook.com>
2024-03-02 16:22:43 +0800

    fix: 修复了触屏下在`ScrollViewer`中无法操作`Slider`的问题

---
+++ ClassIsland/VisualTreeUtils.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media;
+
+namespace ClassIsland;
+
+public static class VisualTreeUtils
+{
+    public static List<T> FindParentVisuals<T>(DependencyObject o) where T: DependencyObject
+    {
+        var parent = VisualTreeHelper.GetParent(o);
+        var r = new List<T>();
+        while (parent != null)
+        {
+            if (parent is T p)
+            {
+                r.Add(p);
+            }
+            parent = VisualTreeHelper.GetParent(parent);
+        }
+
+        return r;
+    }
+}
