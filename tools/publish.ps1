76acacf463eb1d03df2b7787dbac05165fa468b6
WRC <hello_wrc@outlook.com>
2024-03-09 10:46:34 +0800

    fix: 修复构建脚本不能正确上传AppCenter的问题

--- tools/publish.ps1
+++ tools/publish.ps1
@@ -4,7 +4,7 @@ $env:git_latest_tag = git describe --abbrev=0 --tags
 $env:git_commit_short = $(git rev-parse HEAD).substring(0, 7)
 $env:git_current_branch = git branch --show-curren
 echo "Git commit: ${env:git_commit_short} ${env:git_latest_tag} ${$env:git_current_branch}"
-echo "APPVEYOR_REPO_TAG = ${APPVEYOR_REPO_TAG}"
+echo "APPVEYOR_REPO_TAG = ${env:APPVEYOR_REPO_TAG}"
 
 dotnet publish .\ClassIsland\ClassIsland.csproj -c Release -p:PublishProfile=FolderProfile -p:PublishDir=$PUBLISH_TARGET -property:DebugType=embedded
 
@@ -13,8 +13,7 @@ echo "Successfully published to $PUBLISH_TARGET"
 echo "Packaging..."
 7z a ./ClassIsland/ClassIsland/ClassIsland.zip ./ClassIsland/ClassIsland/* -r -mx=9
 
-
-if ($APPVEYOR_REPO_TAG) {
+if ($env:APPVEYOR_REPO_TAG) {
     echo "Uploading to AppCenter..."
     pwsh -ep Bypass -c .\tools\pre-appcenter-upload.ps1
     pwsh -ep Bypass -c .\tools\appcenter-upload.ps1 $appcenter_token ${env:git_latest_tag}
