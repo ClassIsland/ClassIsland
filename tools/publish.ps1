46c220af467555cca7d89638d34eb68b80f2db4a
WRC <hello_wrc@outlook.com>
2024-03-09 09:58:11 +0800

    pref: 优化构建脚本

---
+++ tools/publish.ps1
@@ -0,0 +1,11 @@
+﻿$PUBLISH_TARGET = ".\ClassIsland"
+
+$env:git_latest_tag = git describe --abbrev=0 --tags
+$env:git_commit_short = $(git rev-parse HEAD).substring(0, 7)
+$env:git_current_branch = git branch --show-curren
+echo "Git commit: ${env:git_commit_short} ${env:git_latest_tag} ${$env:git_current_branch}"
+echo "APPVEYOR_REPO_TAG = ${APPVEYOR_REPO_TAG}"
+
+dotnet publish .\ClassIsland\ClassIsland.csproj -c Release -p:PublishProfile=FolderProfile -p:PublishDir=$PUBLISH_TARGET -property:DebugType=embedded
+
+echo "🎉🎉🎉 Successfully published to $PUBLISH_TARGET"
