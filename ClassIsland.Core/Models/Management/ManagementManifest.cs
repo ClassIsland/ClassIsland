dd503232c183c5ed86ae07f4f863735ad981569b
WRC <hello_wrc@outlook.com>
2024-03-10 13:36:34 +0800

    feat: 集控基本模型

---
+++ ClassIsland.Core/Models/Management/ManagementManifest.cs
@@ -0,0 +1,19 @@
+﻿using ClassIsland.Core.Enums;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ClassIsland.Core.Models.Management;
+
+public class ManagementManifest
+{
+    public ReVersionString ClassPlanSource { get; set; } = new();
+
+    public ReVersionString TimeLayoutSource { get; set; } = new();
+
+    public ReVersionString SubjectsSource { get; set; } = new();
+
+    public ReVersionString DefaultSettingsSource { get; set; } = new();
+
+    public ReVersionString PolicySource { get; set; } = new();
+
+    public ManagementServerKind ServerKind { get; set; } = ManagementServerKind.Serverless;
+}
---
+++ ClassIsland.Core/Models/Management/ReVersionString.cs
@@ -0,0 +1,13 @@
+﻿namespace ClassIsland.Core.Models.Management;
+
+public struct ReVersionString
+{
+    public ReVersionString()
+    {
+        
+    }
+
+    public string? Value { get; set; }
+
+    public int Version { get; set; } = 0;
+}
