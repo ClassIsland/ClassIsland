505230fc420088b795f56ede27b5887d4642f411
WRC <hello_wrc@outlook.com>
2024-03-15 18:54:31 +0800

    feat: `ConfigureFileHelper`

---
+++ ClassIsland.Core/Helpers/ConfigureFileHelper.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Text.Json;
+
+namespace ClassIsland.Core.Helpers;
+
+public class ConfigureFileHelper
+{
+    public static bool IsBackupEnabled { get; set; } = true;
+
+    public static T LoadConfig<T>(string path, bool? backupEnabled)
+    {
+        backupEnabled ??= IsBackupEnabled;
+        if (!File.Exists(path))
+        {
+            return Activator.CreateInstance<T>();
+        }
+
+        try
+        {
+            var json = File.ReadAllText(path);
+            var r = JsonSerializer.Deserialize<T>(json);
+            if (r == null) 
+                return Activator.CreateInstance<T>();
+            if (backupEnabled.Value)
+                File.Copy(path, path + ".bak", true);
+            return r;
+        }
+        catch (Exception ex)
+        {
+            if (backupEnabled.Value)
+                return LoadConfig<T>(path + ".bak", false);
+            throw;
+        }
+
+    }
+
+    public static void SaveConfig<T>(string path, T o)
+    {
+        File.WriteAllText(path, JsonSerializer.Serialize<T>(o));
+    }
+
+    public static T CopyObject<T>(T o)
+    {
+        return JsonSerializer.Deserialize<T>(JsonSerializer.Serialize(o)) ?? Activator.CreateInstance<T>();
+    }
+
+    public static IDictionary<TKey, TValue> MergeDictionary<TKey, TValue>(IDictionary<TKey, TValue> raw,
+        IDictionary<TKey, TValue> diffBase, IDictionary<TKey, TValue> diffNew)
+    {
+        var rm = (from i in diffNew.Keys where !diffBase.Keys.Contains(i) select i).ToList();
+        rm.ForEach(i => raw.Remove(i));
+        foreach (var i in diffNew)
+        {
+            raw[i.Key] = i.Value;
+        }
+        return raw;
+    }
+}
