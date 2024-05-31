b8bbd92b0fba1e7e4d58dd31f7cd2639500a1bb1
WRC <hello_wrc@outlook.com>
2024-05-31 20:52:43 +0800

    feat: 从崩溃报告中直接打开issues提交界面

--- ClassIsland/Views/CrashWindow.xaml.cs
+++ ClassIsland/Views/CrashWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Diagnostics;
+using System.Web;
+using System.Windows;
 
 using ClassIsland.Controls;
 
@@ -42,5 +45,19 @@ public partial class CrashWindow : MyWindow
         TextBoxCrashInfo.Focus();
         TextBoxCrashInfo.SelectAll();
         TextBoxCrashInfo.Copy();
+    }
+
+    private void ButtonFeedback_OnClick(object sender, RoutedEventArgs e)
+    {
+        var uri = new UriBuilder(
+            $"https://github.com/ClassIsland/ClassIsland/issues/new");
+        uri.Query = 
+            $"assignees=HelloWRC&labels=bug&projects=&template=BugReport.yml&title=%5BBug%5D%3A+%E5%9C%A8%E8%BF%99%E9%87%8C%E8%BE%93%E5%85%A5%E4%BD%A0%E7%9A%84%E6%A0%87%E9%A2%98&stacktrace={HttpUtility.UrlEncode(CrashInfo)}&app_version={HttpUtility.UrlEncode(App.AppVersionLong)}&os_version={HttpUtility.UrlEncode(Environment.OSVersion.Version.ToString())}";
+        Process.Start(new ProcessStartInfo()
+        {
+            FileName = uri.ToString(),
+            UseShellExecute = true
+        });
+
     }
 }
