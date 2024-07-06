41555eb67181508bf59b847a50ef470a08ff389a
WRC <hello_wrc@outlook.com>
2024-07-06 16:32:57 +0800

    feat: 优化课表列表界面，在课表列表界面显示触发规则及启用状态

---
+++ ClassIsland.Core/Converters/WeekIndexToWeekStringConverter.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace ClassIsland.Core.Converters;
+
+public class WeekIndexToWeekStringConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        var index = value as int? ?? -1;
+        return index switch
+        {
+            0 => "周日",
+            1 => "周一",
+            2 => "周二",
+            3 => "周三",
+            4 => "周四",
+            5 => "周五",
+            6 => "周六",
+            _ => "???"
+        };
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return null;
+    }
+}
