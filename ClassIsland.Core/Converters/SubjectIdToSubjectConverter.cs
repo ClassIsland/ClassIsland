63b38366dde8188d8d70d1241ebb262ca5a16d23
WRC <hello_wrc@outlook.com>
2024-07-07 12:58:46 +0800

    fix: 修复在课表编辑界面中科目下拉框科目名变为GUID 的问题

---
+++ ClassIsland.Core/Converters/SubjectIdToSubjectConverter.cs
@@ -0,0 +1,6 @@
+﻿using ClassIsland.Core.Abstractions.Converters;
+using ClassIsland.Shared.Models.Profile;
+
+namespace ClassIsland.Core.Converters;
+
+public class SubjectIdToSubjectConverter : DictionaryValueAccessConverter<Subject>;
