64135ebd065adfdbab1af081a83e757b96e4040f
WRC <hello_wrc@outlook.com>
2024-08-04 10:58:32 +0800

    ci: 更新 Github 工作流

---
+++ tools/publish-nupkg.ps1
@@ -0,0 +1,27 @@
+param($is_release, $is_trim)
+
+$PUBLISH_TARGET = "..\out\ClassIsland"
+
+if ($(Test-Path ./out) -eq $false) {
+    mkdir out
+} else {
+    rm out/* -Recurse -Force
+}
+$tag = $(git describe --tags --abbrev=0)
+$count = $(git rev-list --count HEAD)
+$ver = [System.Version]::Parse($tag)
+
+if ($is_release) {
+    $version = $ver
+} else {
+    $version = [System.Version]::new(1, 0, 0, $($count -as [int]))
+
+}
+echo $($version -as [string])
+#dotnet clean
+
+dotnet build -c Release -p:Platform="Any CPU" -p:Version=$($version -as [string])
+cp ./**/bin/Release/*.nupkg ./out
+
+Write-Host "Successfully published to $PUBLISH_TARGET" -ForegroundColor Green
+
