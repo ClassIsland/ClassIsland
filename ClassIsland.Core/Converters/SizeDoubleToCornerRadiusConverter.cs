3327c872df19bd202e1eb3c447d719c52aa2d6de
WRC <hello_wrc@outlook.com>
2024-08-18 20:11:30 +0800

    fix: 修复下课倒计时大小不正确的问题

---
+++ ClassIsland.Core/Converters/SizeDoubleToCornerRadiusConverter.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace ClassIsland.Core.Converters;
+
+public class SizeDoubleToCornerRadiusConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is double v)
+        {
+            return v / 2;
+        }
+
+        return null;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return null;
+    }
+}
