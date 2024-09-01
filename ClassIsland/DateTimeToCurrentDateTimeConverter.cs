32608766d0dec41c5085754814020ccdd3ec3900
DryIce-cc <DryIce_cc@163.com>
2024-09-01 15:17:42 +0800

    fix: 修复了调试日期和实际日期不一致可能导致准备上课提醒失效的问题

--- ClassIsland/DateTimeToCurrentDateTimeConverter.cs
+++ ClassIsland/DateTimeToCurrentDateTimeConverter.cs
@@ -1,12 +1,13 @@
 ﻿using System;
-
+using ClassIsland.Services;
+using ClassIsland.Shared;
 namespace ClassIsland;
 
 public static class DateTimeToCurrentDateTimeConverter
 {
     public static DateTime Convert(DateTime dateTime)
     {
-        var now = DateTime.Now;
+        var now = IAppHost.GetService<ExactTimeService>().GetCurrentLocalDateTime();
         return new DateTime(now.Year, now.Month, now.Day, dateTime.Hour, dateTime.Minute,
             dateTime.Second);
     }
