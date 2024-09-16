589d6991e8b3d34cfce0a43c773c6f4c686ad1f9
WRC <hello_wrc@outlook.com>
2024-09-16 21:26:37 +0800

    fix: 修复时间点附加信息上下取整错误的问题，回滚1h整特殊样式

--- ClassIsland.Core/Converters/SecondsToFormatTimeMultiConverter.cs
+++ ClassIsland.Core/Converters/SecondsToFormatTimeMultiConverter.cs
@@ -13,20 +13,20 @@ public class SecondsToFormatTimeMultiConverter : IMultiValueConverter
 {
     public object Convert(object[] value, Type targetType, object parameter, CultureInfo culture)
     {
-        var v0 = TimeSpan.FromSeconds(value[0] as long? ?? 0); // 已过时长或剩余时长
+        if (value.Length < 2)
+        {
+            return "";
+        }
+        var ceiling = (string?)parameter == "1";
+        var v0 = TimeSpan.FromMinutes(Round(TimeSpan.FromSeconds(value[0] as long? ?? 0).TotalMinutes)); // 已过时长或剩余时长
         var v1 = TimeSpan.FromSeconds(value[1] as long? ?? 0); // 总时长
 
-        if (v1 < TimeSpan.FromHours(1))
-            return $"{v0.Minutes}/{v1.Minutes}min"; // 传统样式
-        if (v1 < TimeSpan.FromMinutes(61))
-            return $"{v0.Minutes}/60min"; // 1小时整样式
-        else {
-            var t0 = v0.TotalHours >= 1 ? // 0h03m -> 3m
-                $"{Math.Floor(v0.TotalHours)}h{v0.Minutes:00}m" :
-                $"{v0.Minutes}m";
-            return $"{t0}/{Math.Floor(v1.TotalHours)}h{v1.Minutes:00}m"; // 时分样式
-        }
+        var t0 = v0.TotalHours >= 1 ? // 0h03m -> 3m
+            $"{Math.Floor(v0.TotalHours)}h{v0.Minutes:00}m" :
+            $"{v0.TotalMinutes}m";
+        return $"{t0}/{Math.Floor(v1.TotalHours)}h{v1.Minutes:00}m"; // 时分样式
 
+        double Round(double x) => ceiling ? Math.Ceiling(x) : Math.Floor(x);
     }
 
     public object[] ConvertBack(object value, Type[] targetType, object? parameter, CultureInfo culture) => new object[] { };
