f9cd52b1b0bdf957587652361aed6a8355e7b337
Amiya <mc.amiya@qq.com>
2024-12-15 01:44:12 +0800

    feat(CityInfo): 添加城市信息模型

---
+++ ClassIsland.Core/Models/Weather/CityInfo.cs
@@ -0,0 +1,23 @@
+﻿namespace ClassIsland.Core.Models.Weather;
+using System.Text.Json.Serialization;
+
+/// <summary>
+/// CityInfo 搜索城市或地区的信息。
+/// </summary>
+public class CityInfo
+{
+    /// <summary>
+    /// Name 城市或地区名称。
+    /// </summary>
+    [JsonPropertyName("name")] public string Name { get; set; } = string.Empty;
+    
+    /// <summary>
+    /// Affiliation 城市或地区所属的国家或地区。
+    /// </summary>
+    [JsonPropertyName("affiliation")] public string Affiliation { get; set; } = string.Empty;
+    
+    /// <summary>
+    /// LocationKey 城市或地区的唯一标识符。
+    /// </summary>
+    [JsonPropertyName("locationKey")] public string LocationKey { get; set; } = string.Empty;
+}
