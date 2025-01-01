4739349a9cd3f078d280134de9fcc68b882d5a7a
HelloWRC <hello_wrc@outlook.com>
2025-01-01 18:57:01 +0800

    doc: 补全 CommonDialogBuilder 文档

--- ClassIsland.Core/Controls/CommonDialogBuilder.cs
+++ ClassIsland.Core/Controls/CommonDialogBuilder.cs
@@ -1,34 +1,62 @@
-﻿using System.Windows;
+﻿using ClassIsland.Core.Controls.CommonDialog;
+using MaterialDesignThemes.Wpf;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using ClassIsland.Core.Controls.CommonDialog;
-using MaterialDesignThemes.Wpf;
 
 namespace ClassIsland.Core.Controls;
 
+/// <summary>
+/// <see cref="CommonDialog"/> 构筑类
+/// </summary>
 public class CommonDialogBuilder
 {
+    /// <summary>
+    /// <see cref="CommonDialog"/> 实例
+    /// </summary>
     public CommonDialog.CommonDialog Dialog { get; } = new();
 
+    /// <summary>
+    /// 设置对话框的标题。
+    /// </summary>
+    /// <param name="caption">要设置的标题</param>
+    /// <returns>原来的 <see cref="CommonDialogBuilder"/> 对象</returns>
     public CommonDialogBuilder SetCaption(string caption)
     {
         Dialog.Title = caption;
         return this;
     }
 
+    /// <summary>
+    /// 设置对话框的内容。
+    /// </summary>
+    /// <param name="content">要设置的内容</param>
+    /// <returns>原来的 <see cref="CommonDialogBuilder"/> 对象</returns>
     public CommonDialogBuilder SetContent(string content)
     {
         Dialog.DialogContent = content;
         return this;
     }
 
+    /// <summary>
+    /// 设置自定义图标 <see cref="Visual"/> 对象。
+    /// </summary>
+    /// <param name="visual">自定义的 <see cref="Visual"/> 对象</param>
+    /// <returns>原来的 <see cref="CommonDialogBuilder"/> 对象</returns>
     public CommonDialogBuilder SetIconVisual(Visual? visual)
     {
         Dialog.DialogIcon = visual;
         return this;
     }
 
+    /// <summary>
+    /// 设置自定义位图图标。
+    /// </summary>
+    /// <param name="source">自定义位图 <see cref="BitmapSource"/></param>
+    /// <param name="width">位图宽度（px），默认为 64</param>
+    /// <param name="height">位图高度（px），默认为 64</param>
+    /// <returns>原来的 <see cref="CommonDialogBuilder"/> 对象</returns>
     public CommonDialogBuilder SetBitmapIcon(BitmapSource source, double width = 64, double height = 64)
     {
         Dialog.DialogIcon = new Image()
@@ -40,9 +68,23 @@ public class CommonDialogBuilder
         return this;
     }
 
+    /// <summary>
+    /// 设置自定义位图图标。
+    /// </summary>
+    /// <param name="uri">自定义位图的 Uri</param>
+    /// <param name="width">位图宽度（px），默认为 64</param>
+    /// <param name="height">位图高度（px），默认为 64</param>
+    /// <returns>原来的 <see cref="CommonDialogBuilder"/> 对象</returns>
     public CommonDialogBuilder SetBitmapIcon(Uri uri, double width = 64, double height = 64) =>
         SetBitmapIcon(new BitmapImage(uri), width, height);
 
+    /// <summary>
+    /// 设置自定义 <see cref="PackIcon"/> 图标。
+    /// </summary>
+    /// <param name="kind">自定义图标的图表类型</param>
+    /// <param name="width">图标宽度（px），默认为 64</param>
+    /// <param name="height">图标高度（px），默认为 64</param>
+    /// <returns>原来的 <see cref="CommonDialogBuilder"/> 对象</returns>
     public CommonDialogBuilder SetPackIcon(PackIconKind kind, double width = 64, double height = 64)
     {
         Dialog.DialogIcon = new PackIcon()
@@ -54,18 +96,35 @@ public class CommonDialogBuilder
         return this;
     }
 
+    /// <summary>
+    /// 添加一个操作按钮。
+    /// </summary>
+    /// <param name="action">操作信息</param>
+    /// <returns>原来的 <see cref="CommonDialogBuilder"/> 对象</returns>
     public CommonDialogBuilder AddAction(DialogAction action)
     {
         Dialog.Actions.Add(action);
         return this;
     }
 
+    /// <summary>
+    /// 是否显示输入框。
+    /// </summary>
+    /// <param name="b">是否显示输入框</param>
+    /// <returns>原来的 <see cref="CommonDialogBuilder"/> 对象</returns>
     public CommonDialogBuilder HasInput(bool b)
     {
         Dialog.HasInput = b;
         return this;
     }
 
+    /// <summary>
+    /// 添加一个操作按钮。
+    /// </summary>
+    /// <param name="name">操作名称</param>
+    /// <param name="icon">操作的图标包类型</param>
+    /// <param name="isPrimary">操作是否是主要操作，按下 Enter 时将默认选择</param>
+    /// <returns>原来的 <see cref="CommonDialogBuilder"/> 对象</returns>
     public CommonDialogBuilder AddAction(string name, PackIconKind icon, bool isPrimary=false)
     {
         return AddAction(new DialogAction()
@@ -76,13 +135,41 @@ public class CommonDialogBuilder
         });
     }
 
+    /// <summary>
+    /// 添加一个“确定”操作按钮。
+    /// </summary>
+    /// <returns>原来的 <see cref="CommonDialogBuilder"/> 对象</returns>
     public CommonDialogBuilder AddConfirmAction() => AddAction("确定", PackIconKind.Check);
+
+    /// <summary>
+    /// 添加一个“是”操作按钮。
+    /// </summary>
+    /// <returns>原来的 <see cref="CommonDialogBuilder"/> 对象</returns>
     public CommonDialogBuilder AddYesAction() => AddAction("是", PackIconKind.Check);
+
+    /// <summary>
+    /// 添加一个“否”操作按钮。
+    /// </summary>
+    /// <returns>原来的 <see cref="CommonDialogBuilder"/> 对象</returns>
     public CommonDialogBuilder AddNoAction() => AddAction("否", PackIconKind.Close);
+
+    /// <summary>
+    /// 添加一个“取消”操作按钮。
+    /// </summary>
+    /// <returns>原来的 <see cref="CommonDialogBuilder"/> 对象</returns>
     public CommonDialogBuilder AddCancelAction() => AddAction("取消", PackIconKind.Cancel);
 
+    /// <summary>
+    /// 获得构建的 <see cref="CommonDialog"/> 对象。
+    /// </summary>
+    /// <returns>构建的 <see cref="CommonDialog"/> 对象</returns>
     public CommonDialog.CommonDialog Build() => Dialog;
 
+    /// <summary>
+    /// 显示构建的对话框。
+    /// </summary>
+    /// <param name="owner">对话框所有者</param>
+    /// <returns>对话框选择的返回值</returns>
     public int ShowDialog(Window? owner=null)
     {
         Dialog.Owner = owner;
@@ -90,6 +177,12 @@ public class CommonDialogBuilder
         return Dialog.ExecutedActionIndex;
     }
 
+    /// <summary>
+    /// 显示构建的对话框。
+    /// </summary>
+    /// <param name="inputResult">输入框的输入结果</param>
+    /// <param name="owner">对话框所有者</param>
+    /// <returns>对话框选择的返回值</returns>
     public int ShowDialog(out string inputResult, Window? owner = null)
     {
         var r = ShowDialog(owner);
