3624ce378dca881d347ac7be2f47c4879667740b
HelloWRC <hello_wrc@outlook.com>
2025-01-23 09:51:07 +0800

    fix: 修复 SolidColorBrushToColorConverter 可能出现空值转换的问题

--- ClassIsland.Core/Converters/SolidColorBrushToColorConverter.cs
+++ ClassIsland.Core/Converters/SolidColorBrushToColorConverter.cs
@@ -6,13 +6,13 @@ namespace ClassIsland.Core.Converters;
 
 public class SolidColorBrushToColorConverter : IValueConverter
 {
-    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        var v = (SolidColorBrush)value;
-        return v.Color;
+        var v = value as SolidColorBrush;
+        return v?.Color ?? Color.FromRgb(0, 0, 0);
     }
 
-    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
         return null;
     }
