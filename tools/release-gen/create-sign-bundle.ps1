96a7eaadf8fb071285b7c37f0f7249a392495103
HelloWRC <hello_wrc@outlook.com>
2025-02-04 15:17:06 +0800

    ci: 修复签名 appx 过大的问题

--- tools/release-gen/create-sign-bundle.ps1
+++ tools/release-gen/create-sign-bundle.ps1
@@ -13,3 +13,6 @@ foreach ($artifact in $artifacts) {
     Copy-Item ./out_artifacts/$($artifact.Name)/* -Destination ./out/ -Recurse -Force
     Copy-Item ./out_artifacts/$($artifact.Name)/* -Destination ./ci_tmp/sign_bundle/ -Recurse -Force
 }
+
+Move-Item ./ci_tmp/sign_bundle/out_app_windows_x64_full_wap.appx -Destination ./ci_tmp/sign_bundle/out_app_windows_x64_full_wap.msix
+Remove-Item ./out/out_app_windows_x64_full_wap.appx
