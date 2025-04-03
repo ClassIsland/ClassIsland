2afd84a181dab7b622c2b6769877e444c9dd177f
HelloWRC <hello_wrc@outlook.com>
2025-04-03 19:36:19 +0800

    fix: #905

---
+++ ClassIsland/secrets.g.cs
@@ -0,0 +1,18 @@
+
+namespace ClassIsland.Services.SpeechService{
+    public static partial class GptSovitsSecrets
+    {
+        public const string PrivateKey = 
+"""
+
+""";
+    
+        public const string PrivateKeyPassPhrase = 
+"""
+
+""";
+    
+        public const bool IsSecretsFilled = false;
+    }
+}
+
