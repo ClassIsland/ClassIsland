da63d612e72d634f54392d6defa3830d479adcaa
HelloWRC <hello_wrc@outlook.com>
2025-04-05 09:44:22 +0800

    feat: 在 Release 构建中启用调试菜单的确认

--- ClassIsland/Views/SettingPages/AboutSettingsPage.xaml.cs
+++ ClassIsland/Views/SettingPages/AboutSettingsPage.xaml.cs
@@ -60,13 +60,28 @@ public partial class AboutSettingsPage : SettingsPageBase
     private void AppIcon_OnMouseDown(object sender, MouseButtonEventArgs e)
     {
         ViewModel.AppIconClickCount++;
-        if (ViewModel.AppIconClickCount >= 10)
+        if (ViewModel.AppIconClickCount >= 10 && !SettingsService.Settings.IsDebugOptionsEnabled)
         {
             if (ManagementService.Policy.DisableDebugMenu)
             {
                 CommonDialog.ShowError("调试菜单已被您的组织禁用。");
                 return;
             }
+#if !DEBUG
+            var r1 = new CommonDialogBuilder()
+                .SetPackIcon(PackIconKind.Bug)
+                .SetCaption("启用调试菜单")
+                .SetContent(
+                    "您正在发布版本的 ClassIsland 中启用仅供开发使用的调试菜单。请注意此功能仅限于开发和调试用途，ClassIsland 开发者不对以非开发用途使用此页面中功能造成的任何后果负责，也不接受以非开发用途使用时产生的 Bug 的反馈。\n\n如果您确实要启用此功能，请在下方文本框输入⌈我已知晓并同意，开发者不对以非开发用途使用此页面功能造成的任何后果负责，也不接受以非开发用途使用此页面功能产生的 Bug 的反馈⌋，然后点击【继续】。")
+                .HasInput(true)
+                .AddCancelAction()
+                .AddAction("继续", PackIconKind.ArrowRight, true)
+                .ShowDialog(out var confirm, Window.GetWindow(this));
+            if (r1 != 1 || confirm != "我已知晓并同意，开发者不对以非开发用途使用此页面功能造成的任何后果负责，也不接受以非开发用途使用此页面功能产生的 Bug 的反馈")
+            {
+                return;
+            }
+#endif
             SettingsService.Settings.IsDebugOptionsEnabled = true;
         }
     }
