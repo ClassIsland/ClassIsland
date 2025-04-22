c4543735978474c2e24f4ec09440bd0192240037
Nikoa <95152427+diann34@users.noreply.github.com>
2025-04-22 15:21:08 +0800

    feat: 优化日志中内存使用量的显示、添加初步本地化语言文本 (#956)
    * feat:优化日志记录中内存使用量的显示
    
    * feat:添加本地化语言初步支持
    
    * feat!:更新Nuget包引用至最新版本(除了需要实际代码更改的Nuget包)
    
    * Revert "feat!:更新Nuget包引用至最新版本(除了需要实际代码更改的Nuget包)"
    
    This reverts commit 93dce0683100b87e2cc7f073ebeac27a6bd0829e.
    
    ---------
    
    Co-authored-by: Nikoa <diannwork@outlook.com>

---
+++ ClassIsland/Helpers/StorageSizeHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassIsland.Helpers
+{
+    public static class StorageSizeHelper
+    {
+        public static string FormatSize(long size)
+        {
+            string[] suffixes = { " B", " KiB", " MiB", " GiB", " TiB", " PiB" };
+            double last = 1;
+            for (int i = 0; i < suffixes.Length; i++)
+            {
+                var current = Math.Pow(1024, i + 1);
+                var temp = size / current;
+                if (temp < 1) return (size / last).ToString("n2") + suffixes[i];
+                last = current;
+            }
+            return size.ToString();
+        }
+    }
+}
