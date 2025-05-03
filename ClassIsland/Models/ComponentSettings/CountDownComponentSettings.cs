510c9af47c32e06ba7ef644b385fcf78e4aea4e5
DannyFeng <FengzihangDanny@iCloud.com>
2025-05-03 16:49:10 +0800

    perf: 删除无用的代码

--- ClassIsland/Models/ComponentSettings/CountDownComponentSettings.cs
+++ ClassIsland/Models/ComponentSettings/CountDownComponentSettings.cs
@@ -32,7 +32,6 @@ public class CountDownComponentSettings : ObservableRecipient
         get => _overTime;
         set
         {
-            if (value == null) return;
             if (value.Equals(_overTime)) return;
             _overTime = value;
             OnPropertyChanged();
@@ -55,7 +54,6 @@ public class CountDownComponentSettings : ObservableRecipient
         get => _fontColor;
         set
         {
-            if (value == null) return;
             if (value.Equals(_fontColor)) return;
             _fontColor = value;
             OnPropertyChanged();
@@ -67,7 +65,6 @@ public class CountDownComponentSettings : ObservableRecipient
         get => _fontSize;
         set
         {
-            if (value == null) return;
             if (value.Equals(_fontSize)) return;
             _fontSize = value;
             OnPropertyChanged();
