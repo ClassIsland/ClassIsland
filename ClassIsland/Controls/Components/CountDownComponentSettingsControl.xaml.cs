43b8f1f1c1a3abf83a7f81c97a120c55d33b7030
DannyFeng <FengzihangDanny@iCloud.com>
2025-05-04 21:46:00 +0800

    feat: 增加“自定义”及其编辑

--- ClassIsland/Controls/Components/CountDownComponentSettingsControl.xaml.cs
+++ ClassIsland/Controls/Components/CountDownComponentSettingsControl.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using ClassIsland.Core.Abstractions.Controls;
 using ClassIsland.Models.ComponentSettings;
+using System.Windows.Controls;
 
 namespace ClassIsland.Controls.Components;
 
@@ -8,8 +9,27 @@ namespace ClassIsland.Controls.Components;
 /// </summary>
 public partial class CountDownComponentSettingsControl : ComponentBase<CountDownComponentSettings>
 {
+    private System.Windows.Controls.Control CustomMessageBox;
+
     public CountDownComponentSettingsControl()
     {
         InitializeComponent();
+        CustomMessageBox = new System.Windows.Controls.Control(); // Initialize the control
+    }
+
+    private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        if (sender is ComboBox comboBox && comboBox.SelectedItem is ComboBoxItem selectedItem)
+        {
+            var selectedContent = selectedItem.Content.ToString();
+            if (selectedContent == "自定义")
+            {
+                CustomMessageBox.Visibility = System.Windows.Visibility.Visible;
+            }
+            else
+            {
+                CustomMessageBox.Visibility = System.Windows.Visibility.Collapsed;
+            }
+        }
     }
 }
