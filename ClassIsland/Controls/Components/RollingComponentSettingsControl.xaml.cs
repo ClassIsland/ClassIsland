bcead422045c82d4f08a425a8572bd319bf0115e
HelloWRC <hello_wrc@outlook.com>
2025-05-05 21:28:15 +0800

    feat: 滚动组件 #393

---
+++ ClassIsland/Controls/Components/RollingComponentSettingsControl.xaml.cs
@@ -0,0 +1,54 @@
+﻿using ClassIsland.Core.Abstractions.Controls;
+using ClassIsland.Core.Controls.Ruleset;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClassIsland.Controls.Components;
+
+/// <summary>
+/// RollingComponentSettingsControl.xaml 的交互逻辑
+/// </summary>
+public partial class RollingComponentSettingsControl
+{
+    public RollingComponentSettingsControl()
+    {
+        InitializeComponent();
+    }
+
+    private void OpenDrawer(string key)
+    {
+        if (FindResource(key) is not FrameworkElement drawer)
+        {
+            return;
+        }
+
+        drawer.DataContext = this;
+        SettingsPageBase.OpenDrawerCommand.Execute(drawer);
+    }
+
+    private void ButtonOpenPauseRuleset_OnClick(object sender, RoutedEventArgs e)
+    {
+        if (FindResource("RulesetControl") is RulesetControl rulesetControl)
+            rulesetControl.Ruleset = Settings.PauseRule;
+        OpenDrawer("RulesetControl");
+    }
+
+    private void ButtonOpenStopRuleset_OnClick(object sender, RoutedEventArgs e)
+    {
+        if (FindResource("RulesetControl") is RulesetControl rulesetControl)
+            rulesetControl.Ruleset = Settings.StopRule;
+        OpenDrawer("RulesetControl");
+    }
+}
