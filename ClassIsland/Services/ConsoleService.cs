af8c66fd0b57e6c2bd2fd32508556492189a949c
HelloWRC <hello_wrc@outlook.com>
2025-07-06 16:03:07 +0800

    chore: 修改应用启动 banner

--- ClassIsland/Services/ConsoleService.cs
+++ ClassIsland/Services/ConsoleService.cs
@@ -21,7 +21,7 @@ public class ConsoleService
         AsciiLogo = new StreamReader(s).ReadToEnd();
         Console.WriteLine(AsciiLogo.Pastel("#00bfff"));
         Console.WriteLine($"ClassIsland {App.AppVersionLong}");
-        Console.WriteLine("「钟表的指针周而复始，就像人的困惑、烦恼、软弱…摇摆不停。但最终，人们依旧要前进，就像你的指针，永远落在前方。」".Pastel("#48C0F8"));
+        Console.WriteLine("「赐你，众星俱焚的曙光！」".Pastel("#F4EF74"));
         Console.WriteLine();
     }
 
