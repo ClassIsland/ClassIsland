a29f44ba51633b2fdc0faa36ed223057a1a52178
HelloWRC <hello_wrc@outlook.com>
2025-07-10 22:33:51 +0800

    ci: 修复工件命名问题

--- tools/release-gen/pack/entry.ps1
+++ tools/release-gen/pack/entry.ps1
@@ -4,5 +4,5 @@ $env:version = $(git describe --abbrev=0 --tags)
 
 if ($env:package -eq "folder") {
     ./tools/release-gen/pack/folder.ps1
-    Compress-Archive -Path ./out_pack/pack/* -DestinationPath "./out_pack/out_${build_id}.zip" -Force
+    Compress-Archive -Path ./out_pack/pack/* -DestinationPath "./out_pack/${env:artifact_name}.zip" -Force
 }
