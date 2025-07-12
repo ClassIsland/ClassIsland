49114a4a13333f10b959f61af2748043d222e5db
HelloWRC <hello_wrc@outlook.com>
2025-07-12 14:30:45 +0800

    ci: 更新应用发布流程

--- tools/release-gen/upload.ps1
+++ tools/release-gen/upload.ps1
@@ -23,6 +23,19 @@ $versionInfo.Channels = $tagInfo.Channels
 
 Write-Host "Version: $version"
 
+if ($(Test-Path ./out) -eq $false) {
+    mkdir out
+}
+
+$artifacts = Get-ChildItem -Path ./out_artifacts -Directory
+
+foreach ($artifact in $artifacts) {
+    if ($artifact.Contains("out_app_") -ne $true) {
+        continue
+    }
+    Copy-Item ./out_artifacts/$($artifact.Name)/* -Destination ./out/ -Recurse -Force
+}
+
 foreach ($artifact in $(Get-ChildItem ./out)) {
     Move-Item $artifact.FullName -Destination $artifact.FullName.Replace("out_app_", "ClassIsland_app_") -Force
 }
