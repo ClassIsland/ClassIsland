93f0c9e8005094e0579d2ae413365f59f4647824
HelloWRC <hello_wrc@outlook.com>
2025-07-14 22:36:03 +0800

    fix: 移除平台限定标志

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -6,5 +6,5 @@ using System.Runtime.Versioning;
 [assembly: AssemblyTitle("ClassIsland")]
 [assembly: AssemblyProduct("ClassIsland")]
 #if NETCOREAPP
-[assembly: SupportedOSPlatform("Windows")]
+// [assembly: SupportedOSPlatform("Windows")]
 #endif
