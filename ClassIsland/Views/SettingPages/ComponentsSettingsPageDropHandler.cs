6ec781a050a61dd85012ba03b9a2adca15680b81
HelloWRC <hello_wrc@outlook.com>
2025-07-23 20:26:51 +0800

    fix: 修复组件拖动后乱跑的问题 #1182

--- ClassIsland/Views/SettingPages/ComponentsSettingsPageDropHandler.cs
+++ ClassIsland/Views/SettingPages/ComponentsSettingsPageDropHandler.cs
@@ -1,6 +1,8 @@
 using System.Collections.ObjectModel;
+using System.Linq;
 using Avalonia.Controls;
 using Avalonia.Input;
+using Avalonia.Media;
 using Avalonia.VisualTree;
 using Avalonia.Xaml.Interactions.DragAndDrop;
 using ClassIsland.Core.Attributes;
@@ -8,6 +10,7 @@ using ClassIsland.Core.Models.Components;
 using ClassIsland.Services;
 using ClassIsland.ViewModels.SettingsPages;
 using FluentAvalonia.Core;
+using Visual = Avalonia.Visual;
 
 namespace ClassIsland.Views.SettingPages;
 
@@ -16,6 +19,10 @@ public class ComponentsSettingsPageDropHandler(ComponentsSettingsViewModel viewM
     public ComponentsSettingsViewModel ViewModel { get; } = viewModel;
 
     private ObservableCollection<ComponentSettings>? _sourceCollection;
+
+    private ListBox? _sourceListBox;
+
+    private ListBoxItem? _sourceListBoxItem;
     
     public override void Enter(object? sender, DragEventArgs e, object? sourceContext, object? targetContext)
     {
@@ -84,7 +91,8 @@ public class ComponentsSettingsPageDropHandler(ComponentsSettingsViewModel viewM
             return;
         }
 
-        var listBox = item.FindAncestorOfType<ListBox>();
+        _sourceListBoxItem = item;
+        var listBox = _sourceListBox = item.FindAncestorOfType<ListBox>();
         if (listBox?.ItemsSource is ObservableCollection<ComponentSettings> collection)
         {
             _sourceCollection = collection;
@@ -93,6 +101,21 @@ public class ComponentsSettingsPageDropHandler(ComponentsSettingsViewModel viewM
 
     public void AfterDragDrop(object? sender, PointerEventArgs e, object? context)
     {
+        ClearTransform(_sourceListBoxItem);
+        foreach (var control in _sourceListBox?.Items
+                     .OfType<object>()
+                     .Select(x => _sourceListBox.ContainerFromItem(x)) ?? [])
+        {
+            ClearTransform(control);
+        }
+
         _sourceCollection = null;
+        _sourceListBoxItem = null;
+        _sourceListBox = null;
+    }
+
+    private void ClearTransform(Control? control)
+    {
+        control?.SetValue(Visual.RenderTransformProperty, new TransformGroup());
     }
 }
