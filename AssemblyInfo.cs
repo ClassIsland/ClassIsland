906f3e8a9aeb8f2aab6ee9a85aec3dbc4b45e043
Pizero <zhaory200707@outlook.com>
2025-08-09 07:42:12 +0800

    feat: support nix

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,8 +1,8 @@
 using System.Reflection;
 using System.Runtime.Versioning;
 
-[assembly: AssemblyVersion(ThisAssembly.Git.BaseTag)]
-[assembly: AssemblyInformationalVersion($"{ThisAssembly.Git.BaseTag}+{ThisAssembly.Git.Sha}")]
+[assembly: AssemblyVersion("1.7.104.0")]
+[assembly: AssemblyInformationalVersion("1.7.104.0+ee5031e5c55186c8f2e6b5217c794b5eb678d970")]
 [assembly: AssemblyTitle("ClassIsland")]
 [assembly: AssemblyProduct("ClassIsland")]
 #if NETCOREAPP
