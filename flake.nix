3c192e88e7b83580095b91cc30cc4bb80e357704
Pizero <zhaory200707@outlook.com>
2025-08-09 08:29:51 +0800

    feat(nix): support aarch64 & add x11 dependcies

--- flake.nix
+++ flake.nix
@@ -1,29 +1,38 @@
 {
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-  };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
   outputs =
-    { self, nixpkgs }:
     {
-      packages.x86_64-linux.classisland =
-        let
-          pkgs = import nixpkgs { system = "x86_64-linux"; };
-          classisland = pkgs.callPackage ./classisland.nix { };
-        in
-        classisland;
-      packages.x86_64-linux.default = self.packages.x86_64-linux.classisland;
-      # apps.x86_64-linux.classisland = self.packages.x86_64-linux.classisland
-      devShells.x86_64-linux.default =
-        let
-          pkgs = import nixpkgs { system = "x86_64-linux"; };
-        in
-        pkgs.mkShell {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    # TODO: darwin support
+    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        classisland = pkgs.callPackage ./classisland.nix { };
+      in
+      {
+        packages.default = classisland;
+
+        apps.default = {
+          type = "app";
+          program = "${classisland}/bin/ClassIsland.Desktop";
+        };
+
+        devShells.default = pkgs.mkShell {
           packages = with pkgs; [
             nuget-to-json
             dotnetCorePackages.sdk_8_0-bin
             xorg.libX11
-            nix-output-monitor
+            xorg.libICE
+            xorg.libSM
+            xorg.libXfixes
           ];
         };
-    };
+      }
+    );
 }
