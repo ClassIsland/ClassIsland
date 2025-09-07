2b8246ac849ab3672d90da514ef09e19440d03c6
lrs2187 <liurongshuo2022@outlook.com>
2025-09-07 14:52:09 +0800

    refactor: 解耦 CopyFreeDesktopIconAsync 逻辑

--- ClassIsland/Helpers/ShortcutHelpers.cs
+++ ClassIsland/Helpers/ShortcutHelpers.cs
@@ -34,4 +34,20 @@ public static class ShortcutHelpers
         var final = string.Format(raw, AppBase.AppVersion, AppBase.ExecutingEntrance);
         await File.WriteAllTextAsync(targetPath, final);
     }
+
+    public static async Task CopyFreeDesktopIconAsync()
+    {
+        if (AppBase.Current.PackagingType is "folder" or "folderClassic") // 仅在绿色版下才需要手动复制图标，安装版应当由安装程序将图标复制到系统目录。)
+        {
+            var iconsDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "icons/hicolor/128x128/apps/");
+            if (!Directory.Exists(iconsDir))
+            {
+                Directory.CreateDirectory(iconsDir);
+            }
+
+            await using var src = AssetLoader.Open(new Uri("avares://ClassIsland/Assets/FreedesktopIcons/AppLogo@128w.png"));
+            await using var file = File.OpenWrite(Path.Combine(iconsDir, "classisland.png"));
+            await src.CopyToAsync(file);
+        }
+    }
 }
