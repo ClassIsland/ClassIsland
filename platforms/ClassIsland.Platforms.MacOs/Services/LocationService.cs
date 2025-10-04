ef847c83019a1661d7764390ebc7f700cfb5e63e
wrc <hello_wrc@outlook.com>
2025-10-04 18:37:20 +0800

    fix: 修复 macos 上第一次获取位置获取不到位置的问题

--- platforms/ClassIsland.Platforms.MacOs/Services/LocationService.cs
+++ platforms/ClassIsland.Platforms.MacOs/Services/LocationService.cs
@@ -8,15 +8,15 @@ public class LocationService : ILocationService
 {
     public LocationService()
     {
+        using var manager = new CLLocationManager();
+        manager.RequestWhenInUseAuthorization();
     }
-    
+
     public async Task<LocationCoordinate> GetLocationAsync()
     {
         using var manager = new CLLocationManager();
-        manager.RequestWhenInUseAuthorization();
-        var tokenSource = new CancellationTokenSource();
-        tokenSource.CancelAfter(TimeSpan.FromSeconds(10.0));
         var location = manager.Location ?? new CLLocation();
+        manager.StopUpdatingLocation();
         return new LocationCoordinate()
         {
             Longitude = location.Coordinate.Longitude,
