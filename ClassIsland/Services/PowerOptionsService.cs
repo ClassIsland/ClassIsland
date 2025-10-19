f464d16ae738f0b53dc29edeeb28f117aa66323d
Nikoa <diannwork@outlook.com>
2025-10-19 17:52:12 +0800

    fix:删除未被使用的空文件

--- ClassIsland/Services/PowerOptionsService.cs
+++
@@ -1,9 +0,0 @@
-using ClassIsland.Platforms.Abstraction.Services;
-using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace ClassIsland.Services;
-
-public class PowerOptionsService:ObservableRecipient
-{
-    
-}
