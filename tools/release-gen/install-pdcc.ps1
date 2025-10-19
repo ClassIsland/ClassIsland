7168735a8177ea7b28eaecc37314ed19db97fbe5
wrc <hello_wrc@outlook.com>
2025-10-19 10:44:00 +0800

    ci: 在构建时生成 PDC 文件图

---
+++ tools/release-gen/install-pdcc.ps1
@@ -0,0 +1,12 @@
+$downloadUrl = "https://github.com/ClassIsland/PhainonDistributionCenter/releases/download/${env:PDCC_VERSION}/out_app_linux_x64.zip"
+$zipPath = "./out_app_linux_x64.zip"
+$extractPath = "./pdcc"
+
+if (!(Test-Path $extractPath)) {
+    New-Item -ItemType Directory -Path $extractPath | Out-Null
+}
+
+Invoke-WebRequest -Uri $downloadUrl -OutFile $zipPath
+Expand-Archive -Path $zipPath -DestinationPath $extractPath -Force
+
+chmod +x "$extractPath/PhainonDistributionCenter.Client"
--- tools/release-gen/pack/folder.ps1
+++ tools/release-gen/pack/folder.ps1
@@ -1,15 +1,23 @@
 $ErrorActionPreference = "Stop"
 
 if ($(Test-Path ./out_pack/) -eq $false) {
-    mkdir -p ./out_pack/pack/app-${env:version}/
+    mkdir -p ./out_pack/pack/app-${env:version}-0/
 }
 
 Get-ChildItem -Path ./out
 
+# Install PDCC
+
+./tools/release-gen/pack/install-pdcc.ps1
+
+
 $appBaseName = "out_appBase_${env:osName}_${env:arch}_${env:buildType}_folder"
 $launcherName = "out_launcher_${env:osName}_${env:arch}_aot_singleFile"
 
-Expand-Archive "./out/${appBaseName}.zip" -DestinationPath ./out_pack/pack/app-${env:version}/ -Force
+Expand-Archive "./out/${appBaseName}.zip" -DestinationPath ./out_pack/pack/app-${env:version}-0/ -Force
 Expand-Archive "./out/${launcherName}.zip" -DestinationPath ./out_pack/pack/ -Force
 
 Remove-Item ./out_pack/pack/*.pdb -Force
+
+$env:PDCC_version = ${env:version}
+./pdcc/PhainonDistributionCenter.Client ./phainon.yml GenerateFileMap ./out_pack/pack/
