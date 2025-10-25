34c367ee4fa5c66604a0ed073cac2404ea2c4be8
wrc <hello_wrc@outlook.com>
2025-10-25 12:55:02 +0800

    fix: 修复在 linux 下当自启动目录不存在时无法创建自启动项目的问题 #1355

--- platforms/ClassIsland.Platforms.Linux/Services/DesktopService.cs
+++ platforms/ClassIsland.Platforms.Linux/Services/DesktopService.cs
@@ -1,25 +1,39 @@
 using ClassIsland.Helpers;
 using ClassIsland.Platforms.Abstraction.Services;
+using ClassIsland.Shared;
+using Microsoft.Extensions.Logging;
 
 namespace ClassIsland.Platforms.Linux.Services;
 
 public class DesktopService : IDesktopService
 {
+    private static string StartupPath => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
+        ".config/autostart/cn.classisland.app.desktop");
+    
     public bool IsAutoStartEnabled
     {
         get =>
-            File.Exists(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
-                ".config/autostart/cn.classisland.app.desktop"));
+            File.Exists(StartupPath);
         set
         {
-            var startupPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
-                ".config/autostart/cn.classisland.app.desktop");
-            if (value)
+            try
             {
-                _ = ShortcutHelpers.CreateFreedesktopShortcutAsync(startupPath);
-            } else if (File.Exists(startupPath))
+                var startupPath = StartupPath;
+                if (!Path.Exists(Path.GetDirectoryName(startupPath)))
+                {
+                    Directory.CreateDirectory(Path.GetDirectoryName(startupPath) ?? "");
+                }
+                if (value)
+                {
+                    _ = ShortcutHelpers.CreateFreedesktopShortcutAsync(startupPath);
+                } else if (File.Exists(startupPath))
+                {
+                    File.Delete(startupPath);
+                }
+            }
+            catch (Exception e)
             {
-                File.Delete(startupPath);
+                IAppHost.TryGetService<ILogger<DesktopService>>()?.LogError(e, "无法设置自启动项目：{} ({})", StartupPath, value);
             }
         }
     }
