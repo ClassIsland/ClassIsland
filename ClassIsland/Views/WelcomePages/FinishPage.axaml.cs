b08f7bbfd2c0fb9d0d1733deb6d3ac2bb0d14464
HelloWRC <hello_wrc@outlook.com>
2025-12-14 12:52:36 +0800

    feat(ui/welcome): 添加基本使用指引

--- ClassIsland/Views/WelcomePages/FinishPage.axaml.cs
+++ ClassIsland/Views/WelcomePages/FinishPage.axaml.cs
@@ -1,5 +1,6 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 
 namespace ClassIsland.Views.WelcomePages;
@@ -9,5 +10,22 @@ public partial class FinishPage : UserControl
     public FinishPage()
     {
         InitializeComponent();
+    }
+
+    private void ButtonNext_OnClick(object? sender, RoutedEventArgs e)
+    {
+        Carousel.SelectedIndex++;
+    }
+
+    private void ButtonPrevious_OnClick(object? sender, RoutedEventArgs e)
+    {
+        if (Carousel.SelectedIndex <= 0)
+        {
+            WelcomeWindow.WelcomeNavigateBackCommand.Execute(this);
+        }
+        else
+        {
+            Carousel.SelectedIndex--;
+        }
     }
 }
