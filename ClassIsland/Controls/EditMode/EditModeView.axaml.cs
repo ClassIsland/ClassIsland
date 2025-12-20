14a9025c360cb2bf8fb4ff94799359a42ae7f9d7
HelloWRC <hello_wrc@outlook.com>
2025-12-20 13:06:26 +0800

    feat(mainwindow/edit-mode): 拖拽添加组件

--- ClassIsland/Controls/EditMode/EditModeView.axaml.cs
+++ ClassIsland/Controls/EditMode/EditModeView.axaml.cs
@@ -1,6 +1,9 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using Avalonia.Xaml.Interactions.DragAndDrop;
+using ClassIsland.Core.Controls;
 using ClassIsland.Shared;
 using ClassIsland.ViewModels.EditMode;
 
@@ -8,10 +11,63 @@ namespace ClassIsland.Controls.EditMode;
 
 public partial class EditModeView : UserControl
 {
-    public EditModeViewModel ViewModel = IAppHost.GetService<EditModeViewModel>();
+    public EditModeViewModel ViewModel { get; } = IAppHost.GetService<EditModeViewModel>();
     
     public EditModeView()
     {
         InitializeComponent();
+        DataContext = this;
+    }
+
+    private void OpenDrawer(string key, string? title = null, string? icon = null)
+    {
+        ViewModel.MainDrawerContent = this.FindResource(key);
+        ViewModel.MainDrawerTitle = icon == null
+            ? title
+            : new IconText
+            {
+                Glyph = icon,
+                Text = title ?? ""
+            };
+        ViewModel.MainDrawerState = VerticalDrawerOpenState.Opened;
+    }
+
+    public void OpenComponentsLibDrawer()
+    {
+        OpenDrawer("ComponentsDrawer", "组件库");
+    }
+
+    private void InstanceOnDragEnded()
+    {
+        if (!ViewModel.IsDrawerTempCollapsed)
+        {
+            return;
+        }
+        
+        ViewModel.MainDrawerState = VerticalDrawerOpenState.Opened;
+        ViewModel.IsDrawerTempCollapsed = false;
+    }
+
+    private void InstanceOnDragStarted()
+    {
+        if (ViewModel.MainDrawerState != VerticalDrawerOpenState.Opened)
+        {
+            return;
+        }
+
+        ViewModel.MainDrawerState = VerticalDrawerOpenState.Collapsed;
+        ViewModel.IsDrawerTempCollapsed = true;
+    }
+    
+    private void Control_OnLoaded(object? sender, RoutedEventArgs e)
+    {
+        ManagedDragDropService.Instance.DragStarted += InstanceOnDragStarted;
+        ManagedDragDropService.Instance.DragEnded += InstanceOnDragEnded;
+    }
+
+    private void Control_OnUnloaded(object? sender, RoutedEventArgs e)
+    {
+        ManagedDragDropService.Instance.DragStarted -= InstanceOnDragStarted;
+        ManagedDragDropService.Instance.DragEnded -= InstanceOnDragEnded;
     }
 }
--- ClassIsland/ViewModels/EditMode/EditModeViewModel.cs
+++ ClassIsland/ViewModels/EditMode/EditModeViewModel.cs
@@ -1,8 +1,18 @@
+using ClassIsland.Core.Abstractions.Services;
+using ClassIsland.Core.Controls;
+using ClassIsland.Services;
 using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace ClassIsland.ViewModels.EditMode;
 
-public partial class EditModeViewModel(MainWindow mainWindow) : ObservableObject
+public partial class EditModeViewModel(MainWindow mainWindow, IComponentsService componentsService, SettingsService settingsService) : ObservableObject
 {
     public MainWindow MainWindow { get; } = mainWindow;
+    public IComponentsService ComponentsService { get; } = componentsService;
+    public SettingsService SettingsService { get; } = settingsService;
+
+    [ObservableProperty] private object? _mainDrawerContent;
+    [ObservableProperty] private object? _mainDrawerTitle;
+    [ObservableProperty] private VerticalDrawerOpenState _mainDrawerState;
+    [ObservableProperty] private bool _isDrawerTempCollapsed;
 }
