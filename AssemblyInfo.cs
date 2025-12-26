770a280d46b7428495ecb72fc3050aace8e05896
HelloWRC <hello_wrc@outlook.com>
2025-12-26 22:20:04 +0800

    fix(r/speech/edgetts): 修复 EdgeTTS 无法朗读的问题

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -5,7 +5,11 @@ using System.Runtime.Versioning;
 [assembly: AssemblyVersion("0.0.0.0")]
 [assembly: AssemblyInformationalVersion("NIXBUILD+NIXBUILD_LONG_HASH")]
 #else
+#if DEBUG
+[assembly: AssemblyVersion("2.0.0.999")]
+#else
 [assembly: AssemblyVersion(ThisAssembly.Git.BaseTag)]
+#endif
 [assembly: AssemblyInformationalVersion($"{ThisAssembly.Git.BaseTag}+{ThisAssembly.Git.Sha}")]
 #endif
 [assembly: AssemblyTitle("ClassIsland")]
@@ -16,3 +20,4 @@ using System.Runtime.Versioning;
 #if Platforms_MacOs
 [assembly:SupportedOSPlatform("macos")]
 #endif
+ 
