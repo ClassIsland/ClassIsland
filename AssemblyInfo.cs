d3dce6110ec62a2f738b449205444a1f28d63489
HelloWRC <hello_wrc@outlook.com>
2025-12-27 13:44:43 +0800

    build(git): 使用自行实现的 git 信息生成器取代 GitInfo，修复在特定 git 分支下无法正常构建的问题

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,17 +1,13 @@
 using System.Reflection;
-using System.Runtime.Versioning;
+using ClassIsland;
 
 #if NIX
 [assembly: AssemblyVersion("0.0.0.0")]
 [assembly: AssemblyInformationalVersion("NIXBUILD+NIXBUILD_LONG_HASH")]
-#else
-#if DEBUG
-[assembly: AssemblyVersion("2.0.0.999")]
-#else
-[assembly: AssemblyVersion(ThisAssembly.Git.BaseTag)]
 #endif
-[assembly: AssemblyInformationalVersion($"{ThisAssembly.Git.BaseTag}+{ThisAssembly.Git.Sha}")]
-#endif
+[assembly: AssemblyVersion(GitInfo.Tag)]
+[assembly: AssemblyInformationalVersion($"{GitInfo.Tag}+{GitInfo.CommitHash}")]
+
 [assembly: AssemblyTitle("ClassIsland")]
 [assembly: AssemblyProduct("ClassIsland")]
 #if NETCOREAPP
