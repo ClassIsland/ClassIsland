f9b0e13865b8d86e064bfadab2843628556c4eb7
HelloWRC <hello_wrc@outlook.com>
2025-12-28 11:08:43 +0800

    feat(data-transfer): 迁移时自动替换 url 协议注册和自启动项目，将迁移后的天气图标更改为 lucide

--- ClassIsland/Views/DataTransferPage.axaml.cs
+++ ClassIsland/Views/DataTransferPage.axaml.cs
@@ -150,7 +150,18 @@ public partial class DataTransferPage : UserControl
         settings.ShowEchoCaveWhenSettingsPageLoading = false;
         if (settings.WeatherIconId == "classisland.weatherIcons.materialDesign")
         {
-            settings.WeatherIconId = "classisland.weatherIcons.fluentDesign";
+            settings.WeatherIconId = "classisland.weatherIcons.lucide";
+        }
+
+        if (PlatformServices.DesktopService.IsAutoStartEnabled)
+        {
+            PlatformServices.DesktopService.IsAutoStartEnabled = false;
+            PlatformServices.DesktopService.IsAutoStartEnabled = true;
+        }
+        if (PlatformServices.DesktopService.IsUrlSchemeRegistered)
+        {
+            PlatformServices.DesktopService.IsUrlSchemeRegistered = false;
+            PlatformServices.DesktopService.IsUrlSchemeRegistered = true;
         }
         ConfigureFileHelper.SaveConfig(Path.Combine(CommonDirectories.AppRootFolderPath, "Settings.json"), settings);
     }
