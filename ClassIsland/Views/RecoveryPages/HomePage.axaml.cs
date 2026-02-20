5ecf8e31d943b1de1dab28c53ad2a28815360309
Nikoa <diannwork@outlook.com>
2026-02-20 20:14:31 +0800

    feat:添加AppBase.Current.PackagingType判断逻辑

--- ClassIsland/Views/RecoveryPages/HomePage.axaml.cs
+++ ClassIsland/Views/RecoveryPages/HomePage.axaml.cs
@@ -1,14 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using ClassIsland.Core;
 using ClassIsland.Core.Helpers.UI;
 using ClassIsland.Services;
 using FluentAvalonia.UI.Controls;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
 using Path = System.IO.Path;
 
 namespace ClassIsland.Views.RecoveryPages;
@@ -19,31 +19,33 @@ namespace ClassIsland.Views.RecoveryPages;
 public partial class HomePage : UserControl
 {
     public Frame? MainFrame { get; init; }
-    
+
     public HomePage()
     {
         InitializeComponent();
-        DataContext = this;
-        List<string> validInstallations;
-        try
+        if (AppBase.Current.PackagingType is "folder" or "folderClassic")
         {
-            validInstallations = Directory.GetDirectories(CommonDirectories.AppPackageRoot)
-                .Where(dir =>
-                    Path.GetFileName(dir).StartsWith("app", StringComparison.OrdinalIgnoreCase) &&
-                    !File.Exists(Path.Combine(dir, ".destroy")) &&
-                    !File.Exists(Path.Combine(dir, ".partial")) &&
-                    File.Exists(Path.Combine(dir, "ClassIsland.Desktop" + AppBase.PlatformExecutableExtension)))
-                .ToList();
-        }catch(Exception ex)
-        {
-            this.ShowErrorToast($"无法检测其他安装实例的状态:{ex.Message}"+Environment.NewLine+"\"回滚\"选项将不可用。");
-            validInstallations = new List<string>();
-        }
+            List<string> validInstallations;
+            try
+            {
+                validInstallations = Directory.GetDirectories(CommonDirectories.AppPackageRoot)
+                    .Where(dir =>
+                        Path.GetFileName(dir).StartsWith("app", StringComparison.OrdinalIgnoreCase) &&
+                        !File.Exists(Path.Combine(dir, ".destroy")) &&
+                        !File.Exists(Path.Combine(dir, ".partial")) &&
+                        File.Exists(Path.Combine(dir, "ClassIsland.Desktop" + AppBase.PlatformExecutableExtension)))
+                    .ToList();
+            }
+            catch (Exception ex)
+            {
+                this.ShowErrorToast($"无法检测其他安装实例的状态:{ex.Message}" + Environment.NewLine + "\"回滚\"选项将不可用。");
+                validInstallations = new List<string>();
+            }
 
-        HasMultipleInstallations =validInstallations.Count>1;
+            ButtonRollBack.IsEnabled = validInstallations.Count > 1;
+        }
     }
 
-    public bool HasMultipleInstallations = false;
     private void ButtonContinue_OnClick(object sender, RoutedEventArgs e)
     {
         AppBase.Current.Restart(["-m"]);
