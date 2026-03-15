74738d51401559310356b1120de75c523f7418c7
HelloWRC <hello_wrc@outlook.com>
2026-03-15 09:54:46 +0800

    fix(tutorial): 修复 TutorialControllerWindow 未注册的问题

--- ClassIsland/App.Services.xaml.cs
+++ ClassIsland/App.Services.xaml.cs
@@ -148,6 +148,7 @@ public partial class App
         services.AddSingleton<EditModeView>();
         services.AddTransient<TutorialEditorWindow>();
         services.AddSingleton<TutorialCenterWindow>();
+        services.AddTransient<TutorialControllerWindow>();
         // 设置页面分组
         services.AddSettingsPageGroup("classisland.general", "\uef27", "通用");
         services.AddSettingsPageGroup("classisland.mainwindow", "\uec85", "主界面");
