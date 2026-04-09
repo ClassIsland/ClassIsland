e7d79040335e42919e0c43ecddfc1d281213b241
oldernewbee <316119715@qq.com>
2026-04-09 21:19:42 +0800

    fix(converters): 修复 ControlPreventNullConverter 因静态控件重复使用导致的视觉树冲突

--- ClassIsland.Core/Converters/ElementConverters.cs
+++ ClassIsland.Core/Converters/ElementConverters.cs
@@ -5,11 +5,9 @@ namespace ClassIsland.Core.Converters;
 
 public class ElementConverters
 {
-    private static Border? _border;
 
     public static readonly FuncValueConverter<object?, object> ControlPreventNullConverter = new(x =>
     {
-        _border ??= new Border();
-        return x ?? _border;
+        return x ?? new Border();
     });
 }
