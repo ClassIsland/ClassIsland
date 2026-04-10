d54c600f2bb809cb3c95975ea9bfc9f72b65bfef
oldernewbee <316119715@qq.com>
2026-04-10 14:37:28 +0800

    Remove unnecessary blank line in ElementConverters.cs

--- ClassIsland.Core/Converters/ElementConverters.cs
+++ ClassIsland.Core/Converters/ElementConverters.cs
@@ -5,7 +5,6 @@ namespace ClassIsland.Core.Converters;
 
 public class ElementConverters
 {
-
     public static readonly FuncValueConverter<object?, object> ControlPreventNullConverter = new(x =>
     {
         return x ?? new Border();
