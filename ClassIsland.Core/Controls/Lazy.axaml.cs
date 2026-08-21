2809781ace0f3d2f4d726ba7d826fd20245a0f62
wrc <hello_wrc@outlook.com>
2026-08-21 13:02:04 +0800

    feat(ui): 为部分加载慢的元素启用懒加载

---
+++ ClassIsland.Core/Controls/Lazy.axaml.cs
@@ -0,0 +1,113 @@
+using System.Reactive.Disposables;
+using Avalonia;
+using Avalonia.Animation.Easings;
+using Avalonia.Controls;
+using Avalonia.Controls.Metadata;
+using Avalonia.Controls.Presenters;
+using Avalonia.Controls.Primitives;
+using Avalonia.Interactivity;
+using Avalonia.Rendering.Composition;
+using Avalonia.Threading;
+
+namespace ClassIsland.Core.Controls;
+
+public class Lazy : ContentControl
+{
+    private ContentPresenter? _contentPresenter;
+    private ExpressiveLoadingIndicator? _loadingIndicator;
+    private bool _isContentChangesPending;
+
+    public Lazy()
+    {
+        this.GetObservable(ContentProperty).Subscribe(_ => UpdateContent());
+        this.GetObservable(ContentTemplateProperty).Subscribe(_ => UpdateContent());
+        
+        Loaded += OnLoaded;
+    }
+
+    private void OnLoaded(object? sender, RoutedEventArgs e)
+    {
+        if (_isContentChangesPending)
+        {
+            UpdateContent();
+        }
+    }
+
+    private void UpdateContent()
+    {
+        _isContentChangesPending = true;
+        if (!IsLoaded)
+        {
+            return;
+        }
+
+        _isContentChangesPending = false;
+        SetCompositionOpacity(_contentPresenter, 0f);
+        SetCompositionOpacity(_loadingIndicator, 1f);
+        _loadingIndicator?.IsActive = true;
+        _loadingIndicator?.IsVisible = true;
+        Dispatcher.Post(() =>
+        {
+            _contentPresenter?.Content = Content;
+            _contentPresenter?.ContentTemplate = ContentTemplate;
+            Dispatcher.Post(() =>
+            {
+                SetCompositionOpacity(_contentPresenter, 1f);
+                SetCompositionOpacity(_loadingIndicator, 0f);
+                _loadingIndicator?.IsVisible = false;
+                _loadingIndicator?.IsActive = false;
+            }, DispatcherPriority.Loaded);
+        });
+    }
+
+    protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+    {
+        _contentPresenter = e.NameScope.Find<ContentPresenter>("PART_ContentPresenter");
+        _loadingIndicator = e.NameScope.Find<ExpressiveLoadingIndicator>("PART_LoadingIndicator");
+
+        if (_contentPresenter != null)
+        {
+            SetupImplicitTransitionAnimation(_contentPresenter);
+        }
+        if (_loadingIndicator != null)
+        {
+            SetupImplicitTransitionAnimation(_loadingIndicator);
+        }
+        base.OnApplyTemplate(e);
+    }
+
+    private void SetupImplicitTransitionAnimation(Control control)
+    {
+        var element = ElementComposition.GetElementVisual(control);
+        if (element == null)
+        {
+            return;
+        }
+
+        var compositor = element.Compositor;
+        var opacityAnimation = compositor.CreateScalarKeyFrameAnimation();
+        opacityAnimation.Target = "Opacity";
+        opacityAnimation.InsertExpressionKeyFrame(1.0f, "this.FinalValue", Easing.Parse("0,0 0,1"));
+        opacityAnimation.Duration = TimeSpan.FromMilliseconds(150);
+        var implicitAnimations = compositor.CreateImplicitAnimationCollection();
+        implicitAnimations["Opacity"] = opacityAnimation;
+        element.ImplicitAnimations = implicitAnimations;
+    }
+
+    private void SetCompositionOpacity(Control? control, float opacity)
+    {
+        if (control == null) return;
+        var element = ElementComposition.GetElementVisual(control);
+        if (element == null)
+        {
+            return;
+        }
+
+        var compositor = element.Compositor;
+        var opacityAnimation = compositor.CreateScalarKeyFrameAnimation();
+        opacityAnimation.Target = "Opacity";
+        opacityAnimation.InsertKeyFrame(1.0f, opacity, Easing.Parse("0,0 0,1"));
+        opacityAnimation.Duration = TimeSpan.FromMilliseconds(200);
+        element.StartAnimation("Opacity", opacityAnimation);
+    }
+}
