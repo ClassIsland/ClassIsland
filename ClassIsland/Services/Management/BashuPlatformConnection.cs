1669bac38acfb8e823c233af346020e6f947e722
Stuart-0728 <2023051101095@stu.cqnu.edu.cn>
2026-09-05 11:52:46 +0800

    fix: 补充 ClassIsland.Shared 命名空间引用

--- ClassIsland/Services/Management/BashuPlatformConnection.cs
+++ ClassIsland/Services/Management/BashuPlatformConnection.cs
@@ -7,6 +7,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Avalonia.Threading;
 using ClassIsland.Core.Abstractions.Services.Management;
+using ClassIsland.Shared;
 using ClassIsland.Shared.Abstraction.Services;
 using ClassIsland.Shared.Enums;
 using ClassIsland.Shared.Models.Management;
